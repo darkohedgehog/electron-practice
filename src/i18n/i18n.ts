152b321b4394b3e65e256befa2650ee0af57d880
Darko Živić <zivic.darko79@gmail.com>
2025-02-13 17:08:24 +0100

    `Added translations and updated About page layout`

--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -15,7 +15,13 @@ const resources = {
         },
         titleHome: "Vaša riznica znanja",
         spanHome: "Biblioteka Zaharija Orfelin",
-        paragraphHome: "Prikupljamo i stručno obrađujemo, čuvamo i obnavljamo bibliotečku građu. Više od 20.000 naslova koje smo vredno prikupili i ljubomorno ih čuvamo za vas. Obogatite svoje znanje i upoznajte se sa našom kolekcijom..."
+        paragraphHome: "Prikupljamo i stručno obrađujemo, čuvamo i obnavljamo bibliotečku građu. Više od 20.000 naslova koje smo vredno prikupili i ljubomorno ih čuvamo za vas. Obogatite svoje znanje i upoznajte se sa našom kolekcijom...",
+        titleAbout: "O nama",
+        paragraph1About: "Sa preko 20.000 pažljivo prikupljenih naslova, biblioteka Zaharija Orfelin predstavlja pravo utočište za ljubitelje knjige i istraživače znanja. Naša kolekcija obuhvata bogatstvo literature – od klasičnih dela i savremene beletristike do stručnih i naučnih publikacija.",
+        paragraph2About: "Posetite nas i postanite deo priče koju gradimo godinama – priče o kulturi, obrazovanju i ljubavi prema pisanoj reči. Biblioteka Zaharija Orfelin je više od zbirke knjiga – to je vaš prozor u svet znanja.",
+        paragraph3About: "Zaharije Orfelin je bio jedan od najznačajnijih srpskih pisaca i prevodilaca 18. veka. Njegovo delo je ostavilo neizbrisiv trag u srpskoj kulturi i književnosti, a biblioteka koja nosi njegovo ime predstavlja pravo mesto za sve one koji žele da se upoznaju sa njegovim stvaralaštvom.",
+        paragraph4About: "Naša misija je da sačuvamo kulturno nasleđe i omogućimo pristup znanju svima. Naša vrata su otvorena za sve – dođite i uverite se sami!",
+        paragraph5About: "Biblioteka deluje u okviru Ustanove u oblasti kulture Srpski kulturni centar Vukovar."
       }
     },
     'sr-Cyrl': {
@@ -31,7 +37,8 @@ const resources = {
         },
         titleHome: "Ваша ризница знања",
         spanHome: "Библиотека Захарија Орфелин",
-        paragraphHome: "Прикупљамо и стручно обрађујемо, чувамо и обнављамо библиотечку грађу. Више од 20.000 наслова које смо вредно прикупили и љубоморно их чувамо за вас. Обогатите своје знање и упознајте се са нашом колекцијом..."
+        paragraphHome: "Прикупљамо и стручно обрађујемо, чувамо и обнављамо библиотечку грађу. Више од 20.000 наслова које смо вредно прикупили и љубоморно их чувамо за вас. Обогатите своје знање и упознајте се са нашом колекцијом...",
+        titleAbout: "",
       }
     }
   };
--- src/ui/about/About.tsx
+++ src/ui/about/About.tsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import { useTranslation } from 'react-i18next';
 
 const About = () => {
+  const { t } = useTranslation();
   return (
-    <div className='flex items-center justify-center text-4xl pt-10'>About</div>
+    <div className='ml-72 flex items-center justify-center'>
+      <div className="flex justify-center flex-col items-center px-4 my-24 mx-auto">
+      <h1 className="flex items-center justify-center bg-gradient-to-br from-slate-400 to-slate-700 dark:from-slate-300 dark:to-slate-500 py-6 mb-12 bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-6xl">
+        {t('titleAbout')}
+      </h1>
+      <div className="text-neutral-500 dark:text-gray text-2xl max-w-5xl mx-auto mb-10">
+        {t('paragraph1About')} {" "}
+        <p className="font-bold mt-4">
+        {t('paragraph2About')}
+        </p>
+        <p className='mt-4'>
+        {t('paragraph3About')}
+        </p>
+      </div>
+      <div className="text-neutral-500 dark:text-gray text-2xl max-w-5xl mx-auto mt-4">
+      {t('paragraph4About')}
+        <p className='mt-4 font-bold'>
+        {t('paragraph5About')}
+        </p>
+      </div>
+    </div>
+    </div>
   )
 }
 
