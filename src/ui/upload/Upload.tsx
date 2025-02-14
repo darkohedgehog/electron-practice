df6f9d4d405d08da1bb3b8ca6dc870eda98a6081
Darko Živić <zivic.darko79@gmail.com>
2025-02-14 13:21:33 +0100

    Added new component

--- src/ui/upload/Upload.tsx
+++ src/ui/upload/Upload.tsx
@@ -1,4 +1,6 @@
 import React, { ChangeEvent, useState } from 'react';
+import UploadButton from '../components/ui/UploadButton';
+import { Save } from 'lucide-react';
 
 const Upload = () => {
   console.log(window.api);
@@ -89,23 +91,23 @@ const Upload = () => {
       <div className="text-gray-400">
         <div className="container px-5 py-24 mx-auto">
           <div className="flex flex-col text-center w-full mb-20">
-            <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-white">
+            <h1 className="flex items-center justify-center bg-gradient-to-br from-slate-400 to-slate-700 dark:from-slate-300 dark:to-slate-500 py-6 mb-12 bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-6xl">
               Baza podataka
             </h1>
-            <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
-              Unesite podatke o knjizi.
+            <p className="flex items-center justify-center bg-gradient-to-br from-slate-400 to-slate-700 dark:from-slate-300 dark:to-slate-500 py-6 mb-12 bg-clip-text text-center text-2xl font-medium tracking-tight text-transparent md:text-3xl">
+              Unesite podatke o knjizi
             </p>
           </div>
           <form className="flex flex-wrap -m-2" onSubmit={handleSubmit}>
             {/* Polja za unos teksta */}
             <label className="p-2 lg:w-1/3 md:w-1/2 w-full">
-              <div className="h-full flex items-center border-gray-800 border p-4 rounded-lg">
+              <div className="h-full flex items-center border-accent border p-4 rounded-lg">
                 <div className="flex-grow">
                   <input
                     type='text'
                     placeholder='Naslov na latinici'
                     required
-                    className="w-full py-6 px-6"
+                    className="w-full py-10 px-6"
                     value={titleLat}
                     onChange={(e) => setTitleLat(e.target.value)}
                   />
@@ -113,13 +115,13 @@ const Upload = () => {
               </div>
             </label>
             <label className="p-2 lg:w-1/3 md:w-1/2 w-full">
-              <div className="h-full flex items-center border-gray-800 border p-4 rounded-lg">
+              <div className="h-full flex items-center border-accent border p-4 rounded-lg">
                 <div className="flex-grow">
                   <input
                     type='text'
                     placeholder='Naslov na ćirilici'
                     required
-                    className="w-full py-6 px-6"
+                    className="w-full py-10 px-6"
                     value={titleCyr}
                     onChange={(e) => setTitleCyr(e.target.value)}
                   />
@@ -127,7 +129,7 @@ const Upload = () => {
               </div>
             </label>
             <label className="p-2 lg:w-1/3 md:w-1/2 w-full">
-              <div className="h-full flex items-center border-gray-800 border p-4 rounded-lg">
+              <div className="h-full flex items-center border-accent border p-4 rounded-lg">
                 <div className="flex-grow">
                   <input
                     type='text'
@@ -141,7 +143,7 @@ const Upload = () => {
               </div>
             </label>
             <label className="p-2 lg:w-1/3 md:w-1/2 w-full">
-              <div className="h-full flex items-center border-gray-800 border p-4 rounded-lg">
+              <div className="h-full flex items-center border-accent border p-4 rounded-lg">
                 <div className="flex-grow">
                   <input
                     type='text'
@@ -155,13 +157,13 @@ const Upload = () => {
               </div>
             </label>
             <label className="p-2 lg:w-1/3 md:w-1/2 w-full">
-              <div className="h-full flex items-center border-gray-800 border p-4 rounded-lg">
+              <div className="h-full flex items-center border-accent border p-4 rounded-lg">
                 <div className="flex-grow">
                   <input
                     type='text'
                     placeholder='Opis na latinici'
                     required
-                    className="w-full py-6 px-6"
+                    className="w-full py-10 px-6"
                     value={descriptionLat}
                     onChange={(e) => setDescriptionLat(e.target.value)}
                   />
@@ -169,13 +171,13 @@ const Upload = () => {
               </div>
             </label>
             <label className="p-2 lg:w-1/3 md:w-1/2 w-full">
-              <div className="h-full flex items-center border-gray-800 border p-4 rounded-lg">
+              <div className="h-full flex items-center border-accent border p-4 rounded-lg">
                 <div className="flex-grow">
                   <input
                     type='text'
                     placeholder='Opis na ćirilici'
                     required
-                    className="w-full py-6 px-6"
+                    className="w-full py-10 px-6"
                     value={descriptionCyr}
                     onChange={(e) => setDescriptionCyr(e.target.value)}
                   />
@@ -183,10 +185,8 @@ const Upload = () => {
               </div>
             </label>
             {/* Dugme za odabir slike */}
-            <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
-              <button type="button" onClick={handleSelectFile} className="px-8 py-4 bg-green-600 text-white rounded">
-                Odaberi naslovnu sliku
-              </button>
+            <div className="p-2 lg:w-1/3 md:w-1/2 w-full" onClick={handleSelectFile}>
+              <UploadButton />
             </div>
             {/* Galerija – input type file, multiple 
             <label className="p-2 lg:w-1/3 md:w-1/2 w-full">
@@ -204,13 +204,13 @@ const Upload = () => {
             </label>*/}
             {/* Godina izdanja */}
             <label className="p-2 lg:w-1/3 md:w-1/2 w-full">
-              <div className="h-full flex items-center border-gray-800 border p-4 rounded-lg">
+              <div className="h-full flex items-center border-accent border p-4 rounded-lg">
                 <div className="flex-grow">
                   <input
                     type='text'
                     placeholder='Godina izdanja'
                     required
-                    className="w-full py-6 px-6"
+                    className="w-full py-10 px-6"
                     value={year}
                     onChange={(e) => setYear(e.target.value)}
                   />
@@ -218,10 +218,14 @@ const Upload = () => {
               </div>
             </label>
             {/* Dugme za slanje forme */}
-            <div className="p-2 w-full">
-              <button type="submit" className="px-8 py-4 bg-blue-600 text-white rounded">
-                Sačuvaj knjigu
-              </button>
+            <div className="p-2 w-full my-24 flex items-center justify-center">
+            <button type='submit' className="relative mb-20 inline-flex h-12 w-[200px] overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+             <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+             <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-accent dark:text-accentDark backdrop-blur-3xl gap-3">
+            Sačuvaj knjigu
+              <Save />
+            </span>
+            </button>
             </div>
           </form>
         </div>
